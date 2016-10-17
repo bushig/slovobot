95309cd0c971219e45a28813959f60cbc6642f4a
bushig <bushig@mail.ru>
2016-10-18 02:27:07 +0300

    Added gunicorn

--- config.py
+++ config.py
@@ -2,6 +2,5 @@
 
 TOKEN = '270904280:AAFaEMNwd98yA5dBJXHNXv1rvnmyiO94RnM'
 HOST = ''  # Same FQDN used when generating SSL Cert
-PORT = 8443
 CERT = 'path/to/ssl/server.crt'
 CERT_KEY = 'path/to/ssl/server.key'
