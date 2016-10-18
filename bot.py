a796eb9fd93b70f220903bb38a298d95189dadb6
bushig <bushig@mail.ru>
2016-10-18 03:41:49 +0300

    Refactoring

--- bot.py
+++ bot.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import config
-from handlers import start_single
 
 updater = Updater(config.TOKEN)
 dispatcher = updater.dispatcher
 
+import random
+
+#Function to start singleplayer
+def start_single(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Да начанется синглплеер!")
+    message_handler = MessageHandler(Filters.text, listen_single)
+    dispatcher.add_handler(message_handler)
+
+#Function to listen players messages in singleplayer
+def listen_single(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=random.randint(1,100))
 
 
-start_handler = CommandHandler('singleplayer', start_single)
 
-dispatcher.add_handler(start_handler)
+# Add handlers to dispatcher
+dispatcher.add_handler(CommandHandler('singleplayer', start_single))
 
+#Start server
 updater.start_webhook(listen="0.0.0.0",
                       port=config.PORT,
                       url_path=config.TOKEN)
